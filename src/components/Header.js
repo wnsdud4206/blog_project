5bea30797aac583c6fb74ad9ebd68731dcc3ea8c
wnsdud4206 <66582964+wnsdud4206@users.noreply.github.com>
2022-09-03 14:50:01 +0900

    20220903 blogProject first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeaderEl = styled.div`
+  width: 100%;
+  height: 54px;
+`;
+
+function Header() {
+  return (
+    <HeaderEl></HeaderEl>
+  );
+}
+
+export default Header;
